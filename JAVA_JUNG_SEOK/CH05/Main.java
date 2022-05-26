b1bff614f8a1e2a69b24de06ba34320a740a6a74
sejung_kim <order18291@gmail.com>
2022-05-27 01:40:03 +0900

    참조변수

--- JAVA_JUNG_SEOK/CH05/Main.java
+++ JAVA_JUNG_SEOK/CH05/Main.java
@@ -93,29 +93,31 @@ public class Main {
 
         //ex 18
 
-        int[][] score = {
-                            { 100, 100, 100}
-                            ,{ 20, 20, 20}
-                            ,{ 35, 30, 30}
-                            ,{ 40, 40, 40}
-        };
+        // int[][] score = {
+        //                     { 100, 100, 100}
+        //                     ,{ 20, 20, 20}
+        //                     ,{ 35, 30, 30}
+        //                     ,{ 40, 40, 40}
+        // };
 
-        int sum = 0;
+        // int sum = 0;
 
-        for(int i = 0; i < score.length; i++){
-            for(int j = 0; j <  score[i].length; j++){
-                System.out.printf("score[%d][%d]=%d%n", i, j, score[i][j]);
-            }
-        }
+        // for(int i = 0; i < score.length; i++){
+        //     for(int j = 0; j <  score[i].length; j++){
+        //         System.out.printf("score[%d][%d]=%d%n", i, j, score[i][j]);
+        //     }
+        // }
 
-        for(int[] tmp : score){
-            System.out.println(tmp[0]);
-            for(int i : tmp){
-                sum += i;
-            }
-        }
-        System.out.println("sum = "+sum);
+        // for(int[] tmp : score){
+        //     System.out.println(tmp[0]);
+        //     for(int i : tmp){
+        //         sum += i;
+        //     }
+        // }
+        // System.out.println("sum = "+sum);
         
+
+
 
 
     }
